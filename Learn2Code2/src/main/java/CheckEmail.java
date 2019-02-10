cb7eaeda8ef5d6266a3b1d64a63c07ecf4a1eb05
Zach Thomas <zach.thomas@stdesco.com>
2019-02-10 17:37:54 +1000

    Cleaned code

--- Learn2Code2/src/main/java/CheckEmail.java
+++ Learn2Code2/src/main/java/CheckEmail.java
@@ -28,29 +28,25 @@ public void doPost(HttpServletRequest req, HttpServletResponse resp)
 	  STOPWATCH.reset();
 	  STOPWATCH.start();
 	  
-	  System.out.print("CheckEmail:35: Running \n");
-	  
-	  //---- Load the correct file depending on State ----//	  
+	  // FIXME this comment doesn't make any sense - ZT 
+	  //---- Load the correct file depending on State ----// 	  
 	  String email = req.getParameter("email");
 	  
-	  System.out.print("CheckEmail:35:Checking Adress: " 
-				+ email +"\n");
-	  
-	  //---- initialize the hash-map for response back to web-app ----//
+	  //---- initialise the hash-map for response back to web-app ----//
 	  Map<String, Object> map = new HashMap<String, Object>();	  
 	  int responsecode = 200;
 	  
 	  if(ServletUtils.isValidEmailAddress(email)) {
 		  //email is valid
 		  responsecode = 100;
-	  }
-	  
-	  STOPWATCH.stop();
-	  System.out.print("GetCSVData:54:Time to Process: " 
-			  					+ STOPWATCH.getNanoTime() +"\n");
+	  }	  
 	  
 	  map.put("responsecode", responsecode);
-	  ServletUtils.writeback(resp, map);		  
+	  ServletUtils.writeback(resp, map);	
+	  
+	  STOPWATCH.stop();
+	  LOGGER.info("CheckEmail Time to Process: " 
+			  	  + (STOPWATCH.getNanoTime()/1000000) +"ms \n");
   }
 }	
 

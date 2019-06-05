628a37de2c6c3cbfacced002d241b7e14e407f7a
Zach Thomas <zach.thomas@stdesco.com>
2019-06-05 21:04:17 +1000

    Housekeeping

--- Learn2Code2/src/main/java/Register.java
+++ Learn2Code2/src/main/java/Register.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.search.DateUtil;
 
 /**
  * Written by the yeetster
